eed37d5154cdc2b63beee9de05504b87a090320f
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-11-15 17:45:08 -0300

    studying css grid

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,76 @@
 <template>
-  <h1>Hello World!</h1>
+  <body>
+    <div class="grid-wrapper">
+      <header> header </header>
+      <nav> nav </nav>
+      <section> section </section>
+      <main>
+        <article>  </article>
+        <article>  </article>
+        <article>  </article>
+        <article>  </article>
+        <article>  </article>
+        <article>  </article>
+      </main>
 
-  <input 
-    type="text"
-    placeholder="digite aqui sua tarefa!"
-    v-model="newTask"
-  />
-
-  <button @click="addTask"> add task! </button>
-
-  <ul>
-    <li v-for="task in tasks" :key="task[0]">  
-      <span> {{ task[0] }} </span>
-      <button @click="delTask(task[0])"> deletar </button>
-    </li>
-  </ul>
+      <footer> footer </footer>
+    </div>
+  </body>
 </template>
 
 <script>
-export default {
-  
-  data() {
-    return {
-      newTask:'',
-      tasks: new Map(),
-    };
-  },  
-  
-  methods: {
-    addTask() {
-      if(this.newTask.length > 0) {
-        this.tasks.set(Date.now(), this.newTask);
-      };
-      this.newTask = '';
-    },
-
-    delTask(taskId) {
-      this.tasks.delete(taskId);
-    },
-
-  },
-
-};
 </script>
+
+<style>
+
+.grid-wrapper {
+    display:  grid;
+    grid-template-rows: repeat(4, auto);    
+    grid-template-columns: 240px 1fr;
+    gap:  1rem;
+  }
+  
+  .grid-wrapper>* {
+    min-height: 100px;
+    border-radius: 2px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .grid-wrapper>main {
+    background-color: rgb(92, 92, 92);
+    display: grid;
+    grid-auto-rows: auto;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 1rem;
+  }
+
+  header {
+    background-color: rgb(92, 92, 92);
+    grid-column: 1 / -1;
+    grid-row: 1;
+  } 
+    
+  nav {
+    background-color: rgb(92, 92, 92);
+    grid-row: 2 / -2;
+  }
+     
+  section {
+    background-color: rgb(92, 92, 92);
+    grid-column: 2 / -1;
+  } 
+
+  article {
+    background-color: rgb(107, 107, 107);
+    min-height: 100px;
+  } 
+
+  footer {
+    background-color: rgb(92, 92, 92);
+    grid-column: 1 / -1;
+  }
+
+</style>
