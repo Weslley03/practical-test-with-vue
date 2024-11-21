f6527acd09437a513f434ecf331217a2de219900
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-11-21 09:26:02 -0300

    parcel bundler

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,11 @@
 <template>
-  <body>
-    <div class="grid-wrapper">
-      <header> header </header>
-      <nav> nav </nav>
-      <section> section </section>
-      <main>
-        <article>  </article>
-        <article>  </article>
-        <article>  </article>
-        <article>  </article>
-        <article>  </article>
-        <article>  </article>
-      </main>
-
-      <footer> footer </footer>
-    </div>
-  </body>
+  <h1>Hello World with Parcel and VueJS!</h1>
 </template>
 
 <script>
+
 </script>
 
 <style>
-
-.grid-wrapper {
-    display:  grid;
-    grid-template-rows: repeat(4, auto);    
-    grid-template-columns: 240px 1fr;
-    gap:  1rem;
-  }
-  
-  .grid-wrapper>* {
-    min-height: 100px;
-    border-radius: 2px;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .grid-wrapper>main {
-    background-color: rgb(92, 92, 92);
-    display: grid;
-    grid-auto-rows: auto;
-    grid-template-columns: repeat(3, 1fr);
-    gap: 1rem;
-  }
-
-  header {
-    background-color: rgb(92, 92, 92);
-    grid-column: 1 / -1;
-    grid-row: 1;
-  } 
-    
-  nav {
-    background-color: rgb(92, 92, 92);
-    grid-row: 2 / -2;
-  }
-     
-  section {
-    background-color: rgb(92, 92, 92);
-    grid-column: 2 / -1;
-  } 
-
-  article {
-    background-color: rgb(107, 107, 107);
-    min-height: 100px;
-  } 
-
-  footer {
-    background-color: rgb(92, 92, 92);
-    grid-column: 1 / -1;
-  }
 
 </style>
